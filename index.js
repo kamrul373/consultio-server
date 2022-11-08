018f9873420afe5827a2e6cceed00c8348a801a5
unknown <textile.zero@gmail.com>
2022-11-08 18:38:45 +0600

    mongodb basic configuration by dotenv added

--- index.js
+++ index.js
@@ -2,12 +2,27 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+//Mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // corse & dotenv
 const cors = require("cors")
 require("dotenv").config()
 
 app.use(cors());
 app.use(express.json());
+
+// Mongdb configuration
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbqhd62.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+}
+
+run().catch(error => console.log(error));
+
+
 
 app.get("/", (req, res) => {
     res.send("Consultio server is running !");
